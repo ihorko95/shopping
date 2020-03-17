eb99d245f302829b18f7c2ac19c6bc8d0cf63ef9
Ihor Kolisnyk <ihorko95@gmail.com>
2020-03-17 14:23:04 +0100

    Updates

---
+++ precache-manifest.8b0f8d9b370accf856ef05000c4caad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231dddebb65e58393171970515212ddc",
+    "url": "/shopping/index.html"
+  },
+  {
+    "revision": "ce37b6fd35cf6516d193",
+    "url": "/shopping/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7cd14f104f07239a3bb8",
+    "url": "/shopping/static/js/2.030b92a2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/shopping/static/js/2.030b92a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce37b6fd35cf6516d193",
+    "url": "/shopping/static/js/main.8feba701.chunk.js"
+  },
+  {
+    "revision": "05842cccc404efaebfc8",
+    "url": "/shopping/static/js/runtime-main.24be07d1.js"
+  }
+]);
